a8924a138bcf05b8d169852cdf9bf1917bce04ce
Grant Belcher <gtbelch@gmail.com>
2022-04-03 17:10:14 -0700

    adds state to context

--- client/src/context.js
+++ client/src/context.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Context = React.createContext();
 
 function ContextProvider(props) {
-  console.log(props, "from context provider");
-  return (
-    <Context.Provider value={{ test: 1 }}>{props.children}</Context.Provider>
-  );
+  const [photos, setPhotos] = useState({
+    photos: [],
+  });
+
+  useEffect(() => {
+    console.log("getting photos");
+    getPhotos();
+  }, []);
+
+  const getPhotos = () => {
+    // fetch photos
+
+    // add photos to context state
+    setPhotos((prevState) => {
+      console.log(prevState, "Previous state");
+      return {
+        ...prevState,
+        photos: ["new photos"],
+      };
+    });
+  };
+
+  return <Context.Provider value={photos}>{props.children}</Context.Provider>;
 }
 
 export { ContextProvider, Context };
