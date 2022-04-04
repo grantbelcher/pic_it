8d4f934e59b3e4453c9ad1524a98a0417928c823
Grant Belcher <gtbelch@gmail.com>
2022-04-03 18:44:18 -0700

    refactors icons in Photo

--- client/src/components/Photo/Photo.jsx
+++ client/src/components/Photo/Photo.jsx
@@ -7,14 +7,17 @@ function Photo({ img, className }) {
     setHovering(isHovering);
   };
 
+  const heartIcon = hovering && <i className="ri-heart-line favorite"></i>;
+  const cartIcon = hovering && <i className="ri-add-circle-line cart"></i>;
+
   return (
     <div
       className={`${className} image-container`}
       onMouseOver={() => toggleHover(true)}
       onMouseOut={() => toggleHover(false)}
     >
-      {hovering ? <i className="ri-heart-line favorite"></i> : null}
-      {hovering ? <i className="ri-add-circle-line cart"></i> : null}
+      {heartIcon}
+      {cartIcon}
       <img src={img.url} className="image-grid" />
     </div>
   );
