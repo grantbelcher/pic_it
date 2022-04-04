2b8823912ee549ba68eb3ac8e3515902e59f91fc
Grant Belcher <gtbelch@gmail.com>
2022-04-03 18:39:07 -0700

    adds and track hovering state in Photo component

--- client/src/components/Photo/Photo.jsx
+++ client/src/components/Photo/Photo.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Photo({ img, className }) {
-  console.log(className, "className");
+  const [hovering, setHovering] = useState(false);
+
+  const toggleHover = (isHovering) => {
+    setHovering(isHovering);
+  };
+
   return (
-    <div className={`${className} image-container`}>
+    <div
+      className={`${className} image-container`}
+      onMouseOver={() => toggleHover(true)}
+      onMouseOut={() => toggleHover(false)}
+    >
       <img src={img.url} className="image-grid" />
     </div>
   );
